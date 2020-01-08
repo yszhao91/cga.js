2461cca7cbf17ac3b68409536096c82019a21b1d
josenz <josenz@qq.com>
2020-01-08 09:23:30 +0800

    。。。

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yszhao91/xtorcga.io.git master:gh-pages
+git push -f git@github.com:yszhao91/xtorcga.git master:gh-pages
 
+rm -rf *
 cd -
+git checkout  master
