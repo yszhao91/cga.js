fab19b72f9fa9c59d9de524c0659b142678a92a8
josenz <josenz@qq.com>
2020-01-17 16:46:57 +0800

    xtorcga

--- build/cga.js
+++ build/cga.js
@@ -35,6 +35,12 @@ var CGA = (function (exports) {
       return low + Math.floor(Math.random() * (high - low + 1));
     } // Random float from <low, high> interval
 
+    /**
+     * 生成一个low~high之间的浮点数
+     * @param {*} low 
+     * @param {*} high 
+     */
+
     function randFloat(low, high) {
       return low + Math.random() * (high - low);
     }
@@ -89,6 +95,14 @@ var CGA = (function (exports) {
 
       set height(value) {
         this.y = value;
+      }
+
+      static get UnitX() {
+        return new Vector2(1, 0);
+      }
+
+      static get UnitY() {
+        return new Vector2(0, 1);
       }
 
       set(x, y) {
@@ -395,7 +409,6 @@ var CGA = (function (exports) {
       }
 
     }
-
     function v2() {
       return new Vector2();
     }
@@ -857,7 +870,6 @@ var CGA = (function (exports) {
       _onChangeCallback() {}
 
     }
-
     function quat(x, y, z, w) {
       return new Quaternion(x, y, z, w);
     }
@@ -868,6 +880,26 @@ var CGA = (function (exports) {
         this.y = y || 0;
         this.z = z || 0;
         this.isVector3 = true;
+      }
+
+      static get Up() {
+        return new Vector3$1(0, 1, 0);
+      }
+
+      static get Down() {
+        return new Vector3$1(0, 1, 0);
+      }
+
+      static get UnitX() {
+        return new Vector3$1(1, 0, 0);
+      }
+
+      static get UnitY() {
+        return new Vector3$1(0, 1, 0);
+      }
+
+      static get UnitZ() {
+        return new Vector3$1(0, 0, 1);
       }
 
       set(x, y, z) {
@@ -1205,8 +1237,51 @@ var CGA = (function (exports) {
         return Math.abs(this.x) + Math.abs(this.y) + Math.abs(this.z);
       }
 
-      normalize() {
-        return this.divideScalar(this.length() || 1);
+      normalize(robust = false) {
+        return this.divideScalar(this.length() || 1); // if (robust)
+        // {
+        //   var maxAbsComp = Math.abs(v[0]);
+        //   for (var i = 1; i < N; ++i)
+        //   {
+        //     var absComp = Math.abs(v[i]);
+        //     if (absComp > maxAbsComp)
+        //     {
+        //       maxAbsComp = absComp;
+        //     }
+        //   }
+        //   var length;
+        //   if (maxAbsComp > 0)
+        //   {
+        //     v /= maxAbsComp;
+        //     length = Math.sqrt(Dot(v, v));
+        //     v /= length;
+        //     length *= maxAbsComp;
+        //   }
+        //   else
+        //   {
+        //     length = 0;
+        //     for (var i = 0; i < N; ++i)
+        //     {
+        //       v[i] = 0;
+        //     }
+        //   }
+        //   return length;
+        // }
+        // else
+        // {
+        //   var length = this.length();
+        //   if (length > 0)
+        //   {
+        //     v /= length;
+        //   }
+        //   else
+        //   {
+        //     for (var i = 0; i < N; ++i)
+        //     {
+        //       v[i] = 0;
+        //     }
+        //   }
+        // }
       }
 
       setLength(length) {
@@ -1852,6 +1927,9 @@ var CGA = (function (exports) {
       }
 
     }
+    function v4(x, y, z, w) {
+      return Vector4(x, y, z, w);
+    }
 
     var _vector$1 = v3();
 
@@ -2158,7 +2236,6 @@ var CGA = (function (exports) {
       }
 
     }
-
     function m3() {
       return new Matrix3();
     }
@@ -2193,6 +2270,10 @@ var CGA = (function (exports) {
         te[11] = n43;
         te[15] = n44;
         return this;
+      }
+
+      static get Identity() {
+        return new Matrix4();
       }
 
       identity() {
@@ -2942,7 +3023,6 @@ var CGA = (function (exports) {
     var _matrix = m4();
 
     var _quaternion$1 = quat();
-
     class Euler {
       constructor(x, y, z, order) {
         this._x = x || 0;
@@ -3159,7 +3239,6 @@ var CGA = (function (exports) {
       _onChangeCallback() {}
 
     }
-
     function euler(x, y, z) {
       return Euler(x, y, z);
     }
@@ -3204,6 +3283,10 @@ var CGA = (function (exports) {
       at(amount, target) {
         if (target === undefined) target = new Vector3();
         return target.subVectors(this.end, this.start).multiplyScalar(amount).add(this.start);
+      }
+
+      equals(segment) {
+        return this.p0.equals(segment.p0) && this.p1.equals(segment.p1) || this.p1.equals(segment.p0) && this.p1.equals(segment.p0);
       }
 
       clone() {
@@ -3478,7 +3561,41 @@ var CGA = (function (exports) {
        */
 
 
-      distanceLine(line) {} //---Offset------------------------------------------------------
+      distanceLine(line) {} //---Intersect---------------------------------------------------
+
+
+      intersectSegment(segment) {
+        const result = this.distanceSegment(segment);
+        const resultLine = this.distanceLine(segment);
+        result.interserct = false;
+
+        if (!approximateEqual(this.normal.dot(segment.normal), 1, gPrecision)) {
+          // 平行
+          if (resultLine.distance >= gPrecision) ; else {
+            //共线重叠
+            if (this.equals(segment)) {
+              //# 相等
+              result.equals = true;
+            } else if (result.parameters.every(o => o === 0 || o === 1)) ;
+          }
+        } else {
+          if (result.distance > gPrecision) return result;
+          result.interserct = true; //相交
+
+          if (result.parameters.every(o => o === 0 || o === 1)) ; else if (result.parameters[0] === 0 || result.parameters[0] === 1) {
+            // this线段 在端点上
+            result.splitSegs = [[this], [new Segment(segment.p0, result.closests[1]), new Segment(result.closests[1], segment.p1)]];
+          } else if (result.parameters[1] === 0 || result.parameters[1] === 1) {
+            //segment线段在端点上
+            result.splitSegs = [[new Segment(this.p0, result.closests[0]), new Segment(result.closests[0], this.p1)], segment];
+          } else {
+            // 两个都不在端点上
+            result.splitSegs = [[new Segment(this.p0, result.closests[0]), new Segment(result.closests[0], this.p1)], [new Segment(segment.p0, result.closests[1]), new Segment(result.closests[1], segment.p1)]];
+          }
+        }
+
+        return result;
+      } //---Offset------------------------------------------------------
 
       /**
        * 线段偏移
@@ -3658,7 +3775,134 @@ var CGA = (function (exports) {
         return result;
       }
 
-      distancePlane(plane) {} //---相交-------------
+      distancePlane(plane) {}
+
+      distanceTriangle(triangle) {
+        function Orthonormalize(numInputs, v, robust = false) {
+          if (v && 1 <= numInputs && numInputs <= 3) {
+            var minLength = v[0].length();
+            v[0].normalize();
+
+            for (var i = 1; i < numInputs; ++i) {
+              for (var j = 0; j < i; ++j) {
+                var dot = v[i].dot(v[j]);
+                v[i].sub(v[j].clone().multiplyScalar(dot));
+              }
+
+              var length = v[i].length();
+              v[i].normalize();
+
+              if (length < minLength) {
+                minLength = length;
+              }
+            }
+
+            return minLength;
+          }
+
+          return 0;
+        }
+
+        function ComputeOrthogonalComplement(numInputs, v, robust = false) {
+          if (numInputs === 1) {
+            if (Math.abs(v[0][0]) > Math.abs(v[0][1])) {
+              v[1] = v3(-v[0].z, 0, +v[0].x);
+            } else {
+              v[1] = v3(0, +v[0].z, -v[0].y);
+            }
+            numInputs = 2;
+          }
+
+          if (numInputs == 2) {
+            v[2] = v[0].clone().cross(v[1]);
+            return Orthonormalize(3, v, robust);
+          }
+
+          return 0;
+        }
+
+        const result = {
+          closests: [],
+          parameters: [],
+          triangleParameter: []
+        }; // Test if line intersects triangle.  If so, the squared distance
+        // is zero. 
+
+        var edge0 = triangle.p1.clone().sub(triangle.p0);
+        var edge1 = triangle.p2.clone().sub(triangle.p0);
+        var normal = edge0.clone().cross(edge1).normalize();
+        var NdD = normal.dot(this.direction);
+
+        if (Math.abs(NdD) >= gPrecision) {
+          // The line and triangle are not parallel, so the line
+          // intersects/ the plane of the triangle.
+          var diff = this.origin.clone().sub(triangle.p0);
+          var basis = new Array(3); // {D, U, V}
+
+          basis[0] = this.direction;
+          ComputeOrthogonalComplement(1, basis);
+          var UdE0 = basis[1].dot(edge0);
+          var UdE1 = basis[1].dot(edge1);
+          var UdDiff = basis[1].dot(diff);
+          var VdE0 = basis[2].dot(edge0);
+          var VdE1 = basis[2].dot(edge1);
+          var VdDiff = basis[2].dot(diff);
+          var invDet = 1 / (UdE0 * VdE1 - UdE1 * VdE0); // Barycentric coordinates for the point of intersection.
+
+          var b1 = (VdE1 * UdDiff - UdE1 * VdDiff) * invDet;
+          var b2 = (UdE0 * VdDiff - VdE0 * UdDiff) * invDet;
+          var b0 = 1 - b1 - b2;
+
+          if (b0 >= 0 && b1 >= 0 && b2 >= 0) {
+            // Line parameter for the point of intersection.
+            var DdE0 = this.direction.dot(edge0);
+            var DdE1 = this.direction.dot(edge1);
+            var DdDiff = this.direction.dot(diff);
+            result.lineParameter = b1 * DdE0 + b2 * DdE1 - DdDiff; // Barycentric coordinates for the point of intersection.
+
+            result.triangleParameter[0] = b0;
+            result.triangleParameter[1] = b1;
+            result.triangleParameter[2] = b2; // The intersection point is inside or on the triangle.
+
+            result.closests[0] = this.direction.clone().multiplyScalar(result.lineParameter).add(this.origin);
+            result.closests[1] = edge0.multiplyScalar(b1).add(edge1.multiplyScalar(b2)).add(triangle.p0);
+            result.distance = 0;
+            result.sqrDistance = 0;
+            return result;
+          }
+        } // Either (1) the line is not parallel to the triangle and the
+        // point of intersection of the line and the plane of the triangle
+        // is outside the triangle or (2) the line and triangle are
+        // parallel.  Regardless, the closest point on the triangle is on
+        // an edge of the triangle.  Compare the line to all three edges
+        // of the triangle.
+
+
+        result.distance = +Infinity;
+        result.sqrDistance = +Infinity;
+
+        for (var i0 = 2, i1 = 0; i1 < 3; i0 = i1++) {
+          var segCenter = triangle[i0].clone().add(triangle[i1]).multiplyScalar(0.5);
+          var segDirection = triangle[i1].clone().sub(triangle[i0]);
+          var segExtent = 0.5 * segDirection.length();
+          segDirection.normalize();
+          var segment = new Segment(triangle[i0], triangle[i1]);
+          var lsResult = this.distanceSegment(segment);
+
+          if (lsResult.sqrDistance < result.sqrDistance) {
+            result.sqrDistance = lsResult.sqrDistance;
+            result.distance = lsResult.distance;
+            result.lineParameter = lsResult.parameters[0];
+            result.triangleParameter[i0] = 0.5 * (1 - lsResult.parameters[0] / segExtent);
+            result.triangleParameter[i1] = 1 - result.triangleParameter[i0];
+            result.triangleParameter[3 - i0 - i1] = 0;
+            result.closests[0] = lsResult.closests[0];
+            result.closests[1] = lsResult.closests[1];
+          }
+        }
+
+        return result;
+      } //---相交-------------
 
 
       intersectLine(line) {} //---平行-------------
@@ -3995,9 +4239,9 @@ var CGA = (function (exports) {
           p[1] = (1 - z) * p0[1] + z * p1[1];
         }
 
-        var diff = this.clone().sub(triangle.v0);
-        var edge0 = triangle.v1.clone().sub(triangle.v0);
-        var edge1 = triangle.v2.clone().sub(triangle.v0);
+        var diff = this.clone().sub(triangle.p0);
+        var edge0 = triangle.p1.clone().sub(triangle.p0);
+        var edge1 = triangle.p2.clone().sub(triangle.p0);
         var a00 = edge0.dot(edge0);
         var a01 = edge0.dot(edge1);
         var a11 = edge1.dot(edge1);
@@ -4112,7 +4356,7 @@ var CGA = (function (exports) {
         result.parameter[0] = 1 - p[0] - p[1];
         result.parameter[1] = p[0];
         result.parameter[2] = p[1];
-        result.closest = triangle.v0.clone().add(edge0.multiplyScalar(p[0])).add(edge1.multiplyScalar(p[1]));
+        result.closest = triangle.p0.clone().add(edge0.multiplyScalar(p[0])).add(edge1.multiplyScalar(p[1]));
         result.parameters.push(0, result.rayParameter);
         result.closests.push(this, result.closest);
         diff = this.clone().sub(result.closest);
@@ -4667,108 +4911,36 @@ var CGA = (function (exports) {
         const result = {
           parameters: [],
           closests: [],
+          triangleParameter: [],
           sqrDistance: 0,
           distance: 0
-        }; // segment.GetCenteredForm(segCenter, segDirection, segExtent);
+        };
+        var line = new Line(this.origin, this.origin.clone().add(this.direction)); // DCPQuery < Real, Line3 < Real >, Triangle3 < Real >> ltQuery;
 
-        var segCenter = segment.center;
-        var segDirection = segment.direction;
-        var segExtent = segment.len * 0.5;
-        var diff = this.origin.clone().sub(segCenter);
-        var a01 = -this.direction.dot(segDirection);
-        var b0 = diff.dot(this.direction);
-        var s0, s1;
+        var ltResult = line.distanceTriangle(triangle);
 
-        if (Math.abs(a01) < 1) {
-          // The ray and segment are not parallel.
-          var det = 1 - a01 * a01;
-          var extDet = segExtent * det;
-          var b1 = -diff.dot(segDirection);
-          s0 = a01 * b1 - b0;
-          s1 = a01 * b0 - b1;
-
-          if (s0 >= 0) {
-            if (s1 >= -extDet) {
-              if (s1 <= extDet) // region 0
-                {
-                  // Minimum at interior points of ray and segment.
-                  s0 /= det;
-                  s1 /= det;
-                } else // region 1
-                {
-                  s1 = segExtent;
-                  s0 = Math.max(-(a01 * s1 + b0), 0);
-                }
-            } else // region 5
-              {
-                s1 = -segExtent;
-                s0 = Math.max(-(a01 * s1 + b0), 0);
-              }
-          } else {
-            if (s1 <= -extDet) // region 4
-              {
-                s0 = -(-a01 * segExtent + b0);
-
-                if (s0 > 0) {
-                  s1 = -segExtent;
-                } else {
-                  s0 = 0;
-                  s1 = -b1;
-
-                  if (s1 < -segExtent) {
-                    s1 = -segExtent;
-                  } else if (s1 > segExtent) {
-                    s1 = segExtent;
-                  }
-                }
-              } else if (s1 <= extDet) // region 3
-              {
-                s0 = 0;
-                s1 = -b1;
-
-                if (s1 < -segExtent) {
-                  s1 = -segExtent;
-                } else if (s1 > segExtent) {
-                  s1 = segExtent;
-                }
-              } else // region 2
-              {
-                s0 = -(a01 * segExtent + b0);
-
-                if (s0 > 0) {
-                  s1 = segExtent;
-                } else {
-                  s0 = 0;
-                  s1 = -b1;
-
-                  if (s1 < -segExtent) {
-                    s1 = -segExtent;
-                  } else if (s1 > segExtent) {
-                    s1 = segExtent;
-                  }
-                }
-              }
-          }
+        if (ltResult.lineParameter >= 0) {
+          //最近点在直线前半部分部分，涉嫌方向
+          result.distance = ltResult.distance;
+          result.sqrDistance = ltResult.sqrDistance;
+          result.rayParameter = ltResult.lineParameter;
+          result.triangleParameter[0] = ltResult.triangleParameter[0];
+          result.triangleParameter[1] = ltResult.triangleParameter[1];
+          result.triangleParameter[2] = ltResult.triangleParameter[2];
+          result.closests[0] = ltResult.closests[0];
+          result.closests[1] = ltResult.closests[1];
         } else {
-          // Ray and segment are parallel.
-          if (a01 > 0) {
-            // Opposite direction vectors.
-            s1 = -segExtent;
-          } else {
-            // Same direction vectors.
-            s1 = segExtent;
-          }
-
-          s0 = Math.max(-(a01 * s1 + b0), 0);
+          var ptResult = new Point().copy(this.origin).distanceTriangle(triangle);
+          result.distance = ptResult.distance;
+          result.sqrDistance = ptResult.sqrDistance;
+          result.rayParameter = 0;
+          result.triangleParameter[0] = ptResult.parameter[0];
+          result.triangleParameter[1] = ptResult.parameter[1];
+          result.triangleParameter[2] = ptResult.parameter[2];
+          result.closests[0] = this.origin;
+          result.closests[1] = ptResult.closest;
         }
 
-        result.parameters[0] = s0;
-        result.parameters[1] = s1;
-        result.closests[0] = this.direction.clone().multiplyScalar(s0).add(this.origin);
-        result.closests[1] = segDirection.clone().multiplyScalar(s1).add(segCenter);
-        diff = result.closests[0].clone().sub(result.closests[1]);
-        result.sqrDistance = diff.dot(diff);
-        result.distance = Math.sqrt(result.sqrDistance);
         return result;
       }
       /**
@@ -4803,15 +4975,15 @@ var CGA = (function (exports) {
         this.push(v0, v1, v2);
       }
 
-      get v0() {
+      get p0() {
         return this[0];
       }
 
-      get v1() {
+      get p1() {
         return this[1];
       }
 
-      get v2() {
+      get p2() {
         return this[2];
       }
 
@@ -4871,6 +5043,7 @@ var CGA = (function (exports) {
     exports.toFixed = toFixed;
     exports.v2 = v2;
     exports.v3 = v3;
+    exports.v4 = v4;
 
     return exports;
 
