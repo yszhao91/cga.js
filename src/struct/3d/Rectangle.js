253f9ceff51c0c99a33764ff6a3178e91d3ce4e5
josenz <josenz@qq.com>
2020-03-03 20:40:51 +0800

    xtorcga

--- src/struct/3d/Rectangle.js
+++ src/struct/3d/Rectangle.js
@@ -6,7 +6,7 @@ export class Rectangle extends Array {
         if (!v3)
             v3 = v1.clone().sub(v0).add(v2.clone().sub(v0)).add(v0);
 
-        this.push(v0, v1, v2, v3)
+        this.push(v0, v1, v2, v3);
         var d01 = v1.clone().sub(v0);
         var d03 = v3.clone().sub(v0);
         this.extent = [d01.length() * 0.5, d03.length() * 0.5]
---
+++ src/struct/common/KDTree.js
@@ -0,0 +1,5 @@
+export class KDTree {
+    constructor(data) {
+
+    }
+}
