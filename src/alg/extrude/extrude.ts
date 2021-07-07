98e6e79171bf8bd6080a435b9cce7f3dedf539ca
josenz <josenz@qq.com>
2021-07-07 09:40:30 +0800

    alg

---
+++ src/alg/extrude/extrude.ts
@@ -0,0 +1,19 @@
+/*
+ * @Description  :
+ * @Author       : 赵耀圣
+ * @QQ           : 549184003
+ * @Date         : 2021-07-07 09:21:13
+ * @LastEditTime : 2021-07-07 09:30:00
+ * @FilePath     : \cga.js\src\alg\extrude\extrude.ts
+ */
+
+
+
+export class Extrude {
+
+    constructor() {
+
+    }
+
+
+}
---
+++ src/alg/offset/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description  :
+ * @Author       : 赵耀圣
+ * @QQ           : 549184003
+ * @Date         : 2021-07-07 09:31:04
+ * @LastEditTime : 2021-07-07 09:31:04
+ * @FilePath     : \cga.js\src\alg\offset\index.ts
+ */
