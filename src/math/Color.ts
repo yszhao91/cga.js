6a0d1fa58c27f491804d1ea0d83636ded89848e8
yszhao91 <josenz@qq.com>
2022-08-25 17:17:01 +0800

    @xort/cga

--- src/math/Color.ts
+++ src/math/Color.ts
@@ -1,10 +1,10 @@
 import { EventHandler } from "../render/eventhandler";
 
 export class Color extends EventHandler {
-    _r: number =0.0;
-    _g: number =0.0;
-    _b: number =0.0;
-    _a: number =1.0;
+    _r: number = 0.0;
+    _g: number = 0.0;
+    _b: number = 0.0;
+    _a: number = 1.0;
     constructor(r: number = 0, g: number = 0, b: number = 0, a: number = 1) {
         super();
         this._r = r;
@@ -74,7 +74,6 @@ export class Color extends EventHandler {
     }
 
     setHex(hex: number) {
-
         hex = Math.floor(hex);
         if (hex > 0xffffff) {
             this._r = (hex >> 32 & 255) / 255;
@@ -88,7 +87,50 @@ export class Color extends EventHandler {
         }
 
         return this;
+    }
 
+    setStyle(style: string) {
+        let m: any = /^\#([A-Fa-f\d]+)$/.exec(style)
+        // hex color
+
+        const hex = m[1];
+        const size = hex.length;
+
+        if (size === 3) {
+
+            // #ff0
+            this.r = parseInt(hex.charAt(0) + hex.charAt(0), 16) / 255;
+            this.g = parseInt(hex.charAt(1) + hex.charAt(1), 16) / 255;
+            this.b = parseInt(hex.charAt(2) + hex.charAt(2), 16) / 255;
+
+            return this;
+
+        } else if (size === 6) {
+
+            // #ff0000
+            this.r = parseInt(hex.charAt(0) + hex.charAt(1), 16) / 255;
+            this.g = parseInt(hex.charAt(2) + hex.charAt(3), 16) / 255;
+            this.b = parseInt(hex.charAt(4) + hex.charAt(5), 16) / 255;
+            return this;
+        }
+        else if (size === 4) {
+
+            // #ff0000
+            this.r = parseInt(hex.charAt(0) + hex.charAt(0), 16) / 255;
+            this.g = parseInt(hex.charAt(1) + hex.charAt(1), 16) / 255;
+            this.b = parseInt(hex.charAt(2) + hex.charAt(2), 16) / 255;
+            this.a = parseInt(hex.charAt(3) + hex.charAt(3), 16) / 255;
+            return this;
+        }
+        else if (size === 8) {
+
+            // #ff0000
+            this.r = parseInt(hex.charAt(0) + hex.charAt(1), 16) / 255;
+            this.g = parseInt(hex.charAt(2) + hex.charAt(3), 16) / 255;
+            this.b = parseInt(hex.charAt(4) + hex.charAt(5), 16) / 255;
+            this.a = parseInt(hex.charAt(6) + hex.charAt(7), 16) / 255;
+            return this;
+        }
     }
 
     setRGB(r: number, g: number, b: number, a: number = 1.0) {
@@ -99,7 +141,6 @@ export class Color extends EventHandler {
         this._a = a;
 
         return this;
-
     }
 
     clone() {
@@ -110,12 +151,13 @@ export class Color extends EventHandler {
         this._r = color.r;
         this._g = color.g;
         this._b = color.b;
+        this._a = color.a;
 
         return this;
     }
 
     equals(c: this) {
-        return (c.r === this.r) && (c.g === this.g) && (c.b === this.b);
+        return (c.r === this.r) && (c.g === this.g) && (c.b === this.b) && (c.a === this.a);
     }
 
     toArray(array: number[] = [], offset = 0) {
@@ -129,7 +171,7 @@ export class Color extends EventHandler {
     }
 
     getHex() {
-        return (this.r * 255) << 32 ^ (this.g * 255) << 16 ^ (this.b * 255) << 8 ^ (this.a * 255) << 0;
+        return (this.r * 255) << 24 ^ (this.g * 255) << 16 ^ (this.b * 255) << 8 ^ (this.a * 255) << 0;
     }
 
     toJSON() {
